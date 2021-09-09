e01ac2e5873ea5b9971e2152c4cd7be1e20d8c83
Evan Ang <alethiologist@gmail.com>
2021-09-09 23:05:47 +0800

    expanded it to 14 letters

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,25 @@
     <section class="section">
       <div class="container">
         <div class="columns">
-          <div class="column is-three-quarters"></div>
+          <!-- Output Section -->
+          <div class="column is-three-quarters">
+            <div v-if="words.length > 0">
+              <div v-for="i in 11" :key="i">
+                <div v-if="letterNumbers(i + 3).length !== 0">
+                  <div>{{ i + 3 }} Letters</div>
+                  <div class="">
+                    <div v-for="(word, x) in letterNumbers(i + 3)" :key="x">
+                      <div class="button">
+                        {{ word.word }}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Input Fields -->
           <div class="column">
             <b-field label="Starting Letters">
               <b-input v-model="startLetters"></b-input>
@@ -54,10 +72,111 @@ export default {
       startLetters: '',
       requiredLetters: '',
       excludedLetters: '',
-      wordOption: ['Any', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
       wordLength: 'Any',
+      wordOption: ['Any', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
       words: [],
     }
+  },
+  computed: {
+    remainingLetters() {
+      if (this.wordLength === 'Any') {
+        return 14 - this.startLetters.length
+      } else {
+        return this.wordLength - this.startLetters.length
+      }
+    },
+    startMaxLength() {
+      if (this.wordLength === 'Any') {
+        return 13
+      } else {
+        return this.wordLength - 1
+      }
+    },
+    fullUrl() {
+      return (
+        this.baseUrl +
+        this.startLetters +
+        '?'.repeat(this.remainingLetters) +
+        '&md=p,d'
+      )
+    },
+    urlParams() {
+      const params = []
+      if (this.wordLength === 'Any') {
+        for (let i = this.remainingLetters; i >= 1; i--) {
+          if (i + this.startLetters.length >= 4) {
+            params.push(this.startLetters + '?'.repeat(i) + '&md=p')
+          }
+        }
+      } else {
+        params.push(
+          this.startLetters +
+            '?'.repeat(parseInt(this.wordLength) - this.startLetters.length) +
+            '&md=p'
+        )
+      }
+      return params.reverse()
+    },
+  },
+  methods: {
+    editLetters() {
+      if (this.startLetters.length >= this.wordLength) {
+        this.startLetters = this.startLetters.substring(0, this.wordLength - 1)
+      }
+    },
+    editRequiredLetters() {
+      const maxLength = 13 - this.startLetters.length
+      if (this.requiredLetters.length >= maxLength) {
+        this.requiredLetters = this.requiredLetters.substring(0, maxLength)
+      }
+    },
+    removeNames() {
+      this.words = this.words.filter(
+        (entry) =>
+          entry.tags !== undefined && !entry.tags.find((val) => val === 'prop')
+      )
+    },
+    letterNumbers(x) {
+      return this.words.filter((word) => word.word.length === x)
+    },
+    stringContainsString(word, check) {
+      const arr = [word, check]
+      return arr[1]
+        .toLowerCase()
+        .split('')
+        .every((letter) => {
+          return arr[0].toLowerCase().includes(letter)
+        })
+    },
+    stringLacksString(word, check) {
+      const checkArr = check.split('')
+      const wordArr = []
+      checkArr.forEach((letter) => {
+        wordArr.push(word.includes(letter))
+      })
+      return !wordArr.includes(true)
+    },
+
+    // For Datamuse API
+    async getWords() {
+      this.words = []
+      let words = []
+      for (const i in this.urlParams) {
+        const query = await this.$axios.get(this.baseUrl + this.urlParams[i])
+        words.push(query.data)
+      }
+      words = [].concat
+        .apply([], words)
+        .filter(
+          (entry) =>
+            entry.word.match(/^[A-Z]+$/i) &&
+            entry.score >= 90 &&
+            this.stringContainsString(entry.word, this.requiredLetters) &&
+            this.stringLacksString(entry.word, this.excludedLetters)
+        )
+
+      this.words = words
+    },
   },
 }
 </script>
